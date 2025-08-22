71d411e38c0d09db37a54292325f2ac2805c967d
Francisco <franjonas98@hotmail.com>
2025-08-22 12:58:13 -0300

    add ball base funcs and struct

--- Pong2/Pong2/ball.cpp
+++ Pong2/Pong2/ball.cpp
@@ -1,1 +1,22 @@
 #include "ball.h"
+
+namespace pong2
+{
+	void InitBall(Ball& ball, float initBallPosX, float initBallPosY)
+	{
+		ball.ballColor = RED;
+		ball.ballVel = 0.1f;
+
+		ball.ballCircle.pos.x = (ball.initBallPosX / 2);
+		ball.ballCircle.pos.y = (ball.initBallPosY / 2);
+		ball.ballCircle.rad = 2.5f;
+	}
+
+	void UpdateBall(Ball& ball)
+	{
+	}
+
+	void DrawBall(Ball ball)
+	{
+	}
+}
--- Pong2/Pong2/ball.h
+++ Pong2/Pong2/ball.h
@@ -1,5 +1,27 @@
 #pragma once
-class ball
+
+#include "raylib.h"
+
+namespace pong2
 {
-};
+	struct Circle
+	{
+		Vector2 pos;
+		float rad;
+	};
+
+	struct Ball
+	{
+		float initBallPosX = GetScreenWidth();
+		float initBallPosY = GetScreenHeight();
+
+		Circle ballCircle;
+		Color ballColor;
+		float ballVel;
+	};
+	void InitBall(Ball& ball, float initBallPosX, float initBallPosY);
+	void UpdateBall(Ball& ball);
+	void DrawBall(Ball ball);
+}
+
 
