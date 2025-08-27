80d4e38446150d766b4dfc4eab129646253d1091
Francisco <franjonas98@hotmail.com>
2025-08-27 12:14:27 -0300

    add game general stats

--- Pong2/Pong2/utilities.h
+++ Pong2/Pong2/utilities.h
@@ -1,1 +1,13 @@
+#pragma once
 
+namespace pong2
+{
+	//float defaultVel = 0.1f;
+
+	struct Pong
+	{
+		const int screenWidth = 800;
+		const int screenHeight = 450;
+		bool gameIsPause = false;
+	};
+}
