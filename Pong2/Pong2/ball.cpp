f245102e7c0eea9e79508f6866825cc8e2c35e7e
francisco <franjonas98@hotmail.com>
2025-08-29 12:59:31 -0300

    changed ball hitbox logic

--- Pong2/Pong2/ball.cpp
+++ Pong2/Pong2/ball.cpp
@@ -1,6 +1,7 @@
 #include "ball.h"
 
 #include <ctime>
+#include <math.h>
 
 namespace pong2
 {
@@ -9,57 +10,49 @@ namespace pong2
 	void InitBall(Ball& ball)
 	{
 		Vector2 ballPosition = { GetScreenWidth() / 2.0f, GetScreenHeight() / 2.0f };
-		Vector2 ballSped = { 0.2f, 0.1f };
+		Vector2 ballSped = { 0.1f, 0.1f };
 		ball.ballColor = RED;
 		ball.ballSpeed = ballSped;
 		ball.ballCircle.pos.x = ballPosition.x;
 		ball.ballCircle.pos.y = ballPosition.y;
 		ball.ballCircle.rad = 20.0f;
-		ball.gamePause = true;
 		ball.frameCounter = 0;
 	}
 
 	void UpdateBall(Ball& ball)
+	{	
+			ball.ballCircle.pos.x += ball.ballSpeed.x * 0.2f;
+			ball.ballCircle.pos.y += ball.ballSpeed.y * 0.2f;
+	}
+
+	bool CheckCollisionBallPlayer(Ball& ball, Rectangle playerRec, Color& playerColor)
 	{
-		if (IsKeyPressed(KEY_SPACE))
+		if (ball.ballCircle.pos.x + ball.ballCircle.rad >= playerRec.x &&     //izq
+			ball.ballCircle.pos.x - ball.ballCircle.rad <= playerRec.x + playerRec.width   // derech
+			
+			)
+
 		{
-			ball.gamePause = !ball.gamePause;
-		}
-		if (!ball.gamePause)
-		{
-			ball.ballCircle.pos.x += ball.ballSpeed.x;
-			ball.ballCircle.pos.y += ball.ballSpeed.y;	
+			playerColor = WHITE;
+			ball.ballColor = YELLOW;
+			return true;
+			/*ball.ballSpeed.x *= -1.0f;
+			ball.ballSpeed.y *= -1.0f;*/
+			//ball.ballCircle.pos.x = ball.ballCircle.pos.x + ball.ballCircle.rad /*(playerRec.width / 2)*/;
+			//ball.ballCircle.pos.y = ball.ballCircle.pos.y + ball.ballCircle.rad /*(playerRec.height / 2)*/;
 		}
 		else
 		{
-			ball.frameCounter++;
-		}
-	}
-
-	void CheckCollisionBallPlayer(Ball& ball, Rectangle playerRec)
-	{
-		//chequeo de rebote con los bordes de la arena
-			//rebote derecho e izquierdo bas abajo
-		if (ball.ballCircle.pos.x >= ((playerRec.width / 2) - ball.ballCircle.rad))
-		{
-			ball.ballSpeed.x *= -1.0f;
-		}
-		if (ball.ballCircle.pos.x <= ball.ballCircle.rad + (playerRec.width / 2))
-		{
-			ball.ballSpeed.x *= -1.0f;
-		}
-		//rebote inferior y superior
-		if ((ball.ballCircle.pos.y >= ((playerRec.height / 2) - ball.ballCircle.rad))
-			|| (ball.ballCircle.pos.y <= ball.ballCircle.rad + (playerRec.height / 2)))
-		{
-			ball.ballSpeed.y *= -1.0f;
+			playerColor = BROWN;
+			ball.ballColor = RED;
+			return false;
 		}
 	}
 
 	void CheckCollisionBallArena(Ball& ball, int player1Points, int player2Points)
 	{
 		//chequeo de rebote con los bordes de la arena
-			//rebote derecho e izquierdo bas abajo
+		//rebote derecho e izquierdo bas abajo
 		if (ball.ballCircle.pos.x >= (GetScreenWidth() - ball.ballCircle.rad))
 		{
 			ball.ballSpeed.x *= -1.0f;
@@ -78,13 +71,9 @@ namespace pong2
 		}
 	}
 
-	void DrawBall(Ball ball)
+	void DrawBall(Ball& ball)
 	{
 		DrawCircle(ball.ballCircle.pos.x, ball.ballCircle.pos.y,
 			ball.ballCircle.rad, ball.ballColor);
-		if (ball.gamePause && ((ball.frameCounter / 30) % 2))
-		{
-			DrawText("PAUSED", 350, 200, 20, GRAY);
-		}
 	}
 }
