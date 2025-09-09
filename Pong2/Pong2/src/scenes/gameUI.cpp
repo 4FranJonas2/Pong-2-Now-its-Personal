63bf95bf0f0dde5dcdccfa15bb0b8f76aeb51bea
Francisco <franjonas98@hotmail.com>
2025-09-09 17:05:07 -0300

    added endgame msg when player reach winscore

--- Pong2/Pong2/src/scenes/gameUI.cpp
+++ Pong2/Pong2/src/scenes/gameUI.cpp
@@ -20,4 +20,25 @@ namespace pong2
 		DrawText("Press SPACEBAR", 265, 300, textFont, textColor);
 		DrawText("To Continue", 310, 350, textFont, textColor);
 	}
+	void PrintEndMatchMsg(int player1Score, int player2Score, SceneStatus& gameScene)
+	{
+		int textFont = 30;
+		Color textColor = WHITE;
+		int winScore = 3;
+		int auxPosXPlayer1 = 100;
+		int auxPosXPlayer2 = 470;
+		int auxPosY = 100;
+
+		if (player1Score == winScore)
+		{
+			DrawText("Player 1 WINS ", auxPosXPlayer1, auxPosY, textFont, textColor);
+			DrawText("Player 2  LOSE ", auxPosXPlayer2, auxPosY, textFont, textColor);
+		}
+
+		if (player2Score == winScore)
+		{
+			DrawText("Player 1 LOSE ", auxPosXPlayer1, auxPosY, textFont, textColor);
+			DrawText("Player 2  WINS ", auxPosXPlayer2, auxPosY, textFont, textColor);
+		}
+	}
 }
--- Pong2/Pong2/src/scenes/gameUI.h
+++ Pong2/Pong2/src/scenes/gameUI.h
@@ -9,4 +9,5 @@ namespace pong2
 	void PrintScore(int playerScore, int posX);
 	void PrintArena(int screenWidth, int screenHeight);
 	void PrintPause();
+	void PrintEndMatchMsg(int player1Score, int player2Score, SceneStatus& gameScene);
 }
