7eb14822f8f588f7ecca290dcee4d6e45932d85e
Francisco <franjonas98@hotmail.com>
2025-09-09 12:27:37 -0300

    added scene manager enum to use on the switch

--- Pong2/Pong2/src/utilities.h
+++ Pong2/Pong2/src/utilities.h
@@ -3,8 +3,20 @@
 namespace pong2
 {
 	//float defaultVel = 0.1f;
+	enum class SceneManager
+	{
+		INITGAME = 1,
+		GAMEMENU,
+		GAMEPLAYTWOPLAYERS,
+		GAMEPAUSE,
+		RESETGAME,
+		GAMEEND
+	};
+
 	struct Pong
 	{
+		SceneManager gameManager;
+
 		const int screenWidth = 800;
 		const int screenHeight = 450;
 	};
